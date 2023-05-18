7efd8f304558fd4e42a914db1046729eed469014
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-18 02:06:33 -0300

    feat: oauth com github

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { api } from '@/lib/api'
+
+const ONE_MONTH_IN_SECONDS = 60 * 60 * 24 * 30
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  const redirectURL = new URL('/', request.url)
+
+  const cookieExpiresInSeconds = ONE_MONTH_IN_SECONDS
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+    },
+  })
+}
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,7 +2,10 @@ import { User } from 'lucide-react'
 
 export function SignIn() {
   return (
-    <a className="flex items-center gap-3 text-left transition-colors hover:text-gray-50">
+    <a
+      href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+      className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+    >
       <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
         <User className="h-5 w-5 text-gray-500" />
       </div>
