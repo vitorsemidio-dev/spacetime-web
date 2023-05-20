f431db004dee8f98400be0ec0dea6ede41448fa3
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-20 00:28:48 -0300

    feat: criação e listagem de memória

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import '@/lib/dayjs'
 
 import { cookies } from 'next/headers'
 import {
@@ -48,7 +49,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
---
+++ src/lib/dayjs.ts
@@ -0,0 +1,3 @@
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+dayjs.locale(ptBR)
