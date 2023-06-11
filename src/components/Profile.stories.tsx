683dbd22916a29ca9447ab864bd5f53b1d63a9db
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-06-11 17:43:37 -0300

    refactor: remove useless code from profile story

--- src/components/Profile.stories.tsx
+++ src/components/Profile.stories.tsx
@@ -1,28 +1,28 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
-
-import { Profile } from './Profile'
-
-// Mock
-
+import { Meta, StoryObj } from '@storybook/react'
 import { createMock } from 'storybook-addon-module-mock'
+
 import * as auth from '../lib/auth'
+import { Profile } from './Profile'
+import { StorybookWrapper } from './StorybookWrapper'
 
-// jest.mock('../lib/auth')
+type Story = StoryObj<typeof Profile>
 
-const meta: ComponentMeta<typeof Profile> = {
+const meta: Meta<typeof Profile> = {
   title: 'Components/Profile',
   component: Profile,
+  tags: ['components'],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <StorybookWrapper>
+        <Story />
+      </StorybookWrapper>
+    ),
+  ],
 }
 export default meta
 
-// export const Primary: ComponentStoryObj<typeof Profile> = {
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement)
-//     expect(canvas.getByText('Before')).toBeInTheDocument()
-//   },
-// }
-
-export const Mock: ComponentStoryObj<typeof Profile> = {
+export const Mock: Story = {
   parameters: {
     moduleMock: {
       mock: () => {
@@ -36,52 +36,4 @@ export const Mock: ComponentStoryObj<typeof Profile> = {
       },
     },
   },
-  // play: async ({ canvasElement, parameters }) => {
-  //   const canvas = within(canvasElement)
-  //   expect(canvas.getByText('After')).toBeInTheDocument()
-  //   const mock = getMock(parameters, React, 'useMemo')
-  //   expect(mock).toBeCalled()
-  // },
 }
-
-export const Action: ComponentStoryObj<typeof Profile> = {
-  // parameters: {
-  //   moduleMock: {
-  //     mock: () => {
-  //       const mock = createMock(React, 'useMemo')
-  //       return [mock]
-  //     },
-  //   },
-  // },
-  // play: async ({ canvasElement, parameters }) => {
-  //   const canvas = within(canvasElement)
-  //   const mock = getMock(parameters, React, 'useMemo')
-  //   mock.mockReturnValue('Action')
-  //   userEvent.click(await canvas.findByRole('button'))
-  //   await waitFor(() => {
-  //     expect(canvas.getByText('Action')).toBeInTheDocument()
-  //   })
-  // },
-}
-
-// const meta: Meta<typeof Profile> = {
-//   title: 'Components/Profile',
-//   component: Profile,
-//   tags: ['components'],
-//   argTypes: {},
-//   decorators: [
-//     (Story) => (
-//       <StorybookWrapper>
-//         <Story />
-//       </StorybookWrapper>
-//     ),
-//   ],
-// }
-
-// export default meta
-
-// type Story = StoryObj<typeof Profile>
-
-// export const Default: Story = {
-//   args: {},
-// }
